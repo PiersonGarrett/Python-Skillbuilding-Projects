d81b81f5aa91603c859d69ff3d518a13a2c6330c
Kairos <gapi7697@colorado.edu>
2020-08-21 16:54:41 -0600

    Added folder for file manipulation scripts

---
+++ File_Projects/snapshots_to_folder.py
@@ -0,0 +1,4 @@
+# Snapshots on Mac automatically save to the desktop which makes my desktop very messy and I'm sick of it. This program will search my desktop for these screenshots and move them to a screenshot folder on my desktop. I am going to keep it realatively general so it should work on MacOS or Windows (not sure where screenshots on Windows are stored...)
+
+# TODO: Define Problem and Necessary Components and make more todos based on that
+# TIME: 1 Day to complete
